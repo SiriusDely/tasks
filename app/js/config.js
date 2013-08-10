c6616a441d2d8a4323ad7e06f5e0bc07cca86c0c
Sirius Dely <siriusdely@icloud.com>
2013-08-10 12:59:58 +0700

    [++] Part 2 - Done

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function() {
+  var config = {};
+  config.apiKey = 'AIzaSyB0HYfCKumaeYYKY17kzhsO9Ie5F50hkBQ';
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientId = '1071091425154.apps.googleusercontent.com';
+  return config;
+});
