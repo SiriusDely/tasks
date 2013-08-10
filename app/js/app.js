7fcf35bbd1798a4b936a3b82883c07f11fc7e416
Sirius Dely <siriusdely@icloud.com>
2013-08-10 14:53:41 +0700

    [++] Part 5 - Done

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,11 @@ define([
   'gapi'
 , 'views/app'
 , 'views/auth'
+, 'views/lists/menu'
 , 'collections/tasklists'
 ],
 
-function(ApiManager, AppView, AuthView, TaskLists) {
+function(ApiManager, AppView, AuthView, ListMenuView, TaskLists) {
   var App = function() {
     this.views.app = new AppView();
     this.views.app.render();
@@ -14,6 +15,7 @@ function(ApiManager, AppView, AuthView, TaskLists) {
     this.views.auth.render();
 
     this.collections.lists = new TaskLists();
+    this.views.listMenu = new ListMenuView({ collection: this.collections.lists });
 
     this.connectGapi();
   };
@@ -26,9 +28,7 @@ function(ApiManager, AppView, AuthView, TaskLists) {
       this.apiManager = new ApiManager(this);
       this.apiManager.on('ready', function() {
         self.collections.lists.fetch({ data: { userId: '@me' }, success: function(res) {
-          _.each(res.models, function(model) {
-            console.log(model.get('title'));
-          });
+          self.views.listMenu.render();
         }});
       });
     }
